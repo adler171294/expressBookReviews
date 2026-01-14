15e3fb7ee5506fbe034c3c1ed7177bf4bff1e159
Muhammad Fachri <mfachri02@gmail.com>
2026-01-14 15:51:49 +0700

    Add getallbooks for Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,10 +58,17 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review based on ISBN
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;  // Ambil ISBN dari URL
+    const book = books[isbn];
+
+    if (book) {
+        return res.status(200).send(JSON.stringify(book.reviews, null, 4));
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
+
 
 module.exports.general = public_users;
