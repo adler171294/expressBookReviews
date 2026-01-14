73d9090b3334da4849b3c55c841d00bc3446dcf2
adler171294 <mfachri02@gmail.com>
2026-01-14 02:48:38 -0500

    task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,10 +18,17 @@ public_users.get('/', function (req, res) {
 })
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;  // Ambil ISBN dari parameter URL
+    const book = books[isbn];      // Ambil data buku dari booksdb.js
+
+    if (book) {
+        return res.status(200).send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
