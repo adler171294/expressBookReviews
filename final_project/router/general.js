c35308f867a79711a75eac89bbaa55e094a3d83e
Muhammad Fachri <mfachri02@gmail.com>
2026-01-14 15:57:25 +0700

    fix

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,23 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    const userExists = users.find(user => user.username === username);
+
+    if (userExists) {
+        return res.status(409).json({ message: "Username already exists" });
+    }
+
+    users.push({ username, password });
+    return res.status(200).json({ message: "User registered successfully" });
 });
+
 
 // ==================== TASK 1 ====================
 // Get the book list available in the shop
